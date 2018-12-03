ebc092c0e8c050fc2c7fec7c291898bda1536a1f
Caio Queiroz <caiogqueiroz@gmail.com>
2018-12-03 16:12:42 -0300

    attack implemented

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ new Vue({
             user: 100,
             monster: 100
         },
-        gameIsRunning: false
+        gameIsRunning: false,
+        attacks: {
+            user: [],
+            monster: []
+        },
+        turns: []
     },
     computed: {
         lifeUser: function () {
@@ -19,7 +24,17 @@ new Vue({
             this.gameIsRunning = true;
         },
         attack: function () {
-            this.life.user = 80;
+            var damage = 0;
+            //value damaged by the user
+            damage = Math.floor((Math.random() * 15) + 1);
+            this.life.monster = this.life.monster - damage;
+            this.attacks.user.push(damage);
+            this.turns.push(-damage);
+            //value damaged by the monster
+            damage = Math.floor((Math.random() * 15) + 1);
+            this.life.user = this.life.user - damage;
+            this.attacks.monster.push(damage);
+            this.turns.push(-damage);
         }
     }
 });
