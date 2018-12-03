d1dc73ad2f63f1851a9faf8a540147591e4a58ec
Caio Queiroz <caiogqueiroz@gmail.com>
2018-12-03 15:09:29 -0300

    new game implemented

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ new Vue({
         life: {
             user: 100,
             monster: 100
+        },
+        gameIsRunning: false
+    },
+    computed: {
+        lifeUser: function () {
+            return this.user.width = this.life.user;
         }
     },
     methods: {
         newGame: function () {
             this.life.user = 100;
             this.life.monster = 100;
+            this.gameIsRunning = true;
+        },
+        attack: function () {
+            this.life.user = 80;
         }
     }
 });
